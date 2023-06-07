2ed976fe271f2579240117505c7281ee1686c3ad
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2023-06-07 20:06:10 +0600

    fix: assembly language lab 2020 problem display smaller.
    1.input two number from user and compare these for display smaller number between these two numbers.

---
+++ four/assembly/asm2020/q7smaller.asm
@@ -0,0 +1,32 @@
+#dispay smaller number between two inputed number
+
+.model small
+.stack 100h
+.code
+main proc
+   mov ah,01
+   int 21h
+   mov bl,al
+
+   mov ah,01
+   int 21h
+   mov bh,al
+
+   cmp bl,bh
+   jg level
+
+   mov ah,02
+   mov dl,bl
+   int 21h
+   jmp exit
+
+   level:
+   mov ah,02
+   mov dl,bh
+   int 21h
+
+   exit:
+   mov ah,4ch
+   int 21h
+   main endp
+end main
