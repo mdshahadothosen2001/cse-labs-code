7f19ee42bcb7b69bf8fe21fd0e8002b6af158440
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2023-06-12 18:28:55 +0600

    fix: test odd or even number.
    1.take a single decimal number and  test is odd or even then print message.

---
+++ four/assembly/asm2021/odd-even.asm
@@ -0,0 +1,40 @@
+# Display odd or even if number is
+
+.model small
+print macro msg
+   mov ah,09
+   lea dx,msg
+   int 21h
+.stack 100h
+.data
+a db 'Enter number: $'
+od db 10,13,'Number is Even$'
+ev db 10,13,'Number is Odd$'
+n db ?
+.Code
+main proc
+   mov ax,@data
+   mov ds,ax
+   
+   print a 
+
+   mov ah,01
+   int 21h
+   mov n,al
+
+   mov ax,n 
+   div 2
+   cmp al,0
+   je level
+
+   print od
+
+   level:
+   print ev
+
+   exit:
+   mov ah,4ch
+   int 21h
+
+   main endp
+end main
