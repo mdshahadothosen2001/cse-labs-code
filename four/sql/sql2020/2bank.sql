3f628fc03c6eb00350fa495ed5090f6ca0452030
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2023-06-18 22:50:47 +0600

    lab code sql.

--- four/sql/sql2020/2bank.sql
+++
@@ -1,8 +0,0 @@
-a. find all customers who have account but not loan:
-   SELECT d.customer_name
-   FROM depositor d inner join account a 
-   ON d.account_number = a.account_number
-   WHERE d.customer_name not in (
-    SELECT customer_name from borrower b inner join
-    loan l ON b.loan_number = l.loan_number
-   );
